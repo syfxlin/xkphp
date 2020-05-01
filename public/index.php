4a8136e33dc076d1fe9f43372b937452c9ba9895
syfxlin <syfxlin@gmail.com>
2020-05-01 17:31:30 +0800

    fix: :bug:  Fix require

--- public/index.php
+++ public/index.php
@@ -2,4 +2,4 @@
 define('BASE_PATH', dirname(__DIR__) . '/');
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../App/init.php';
+require_once __DIR__ . '/../app/init.php';
